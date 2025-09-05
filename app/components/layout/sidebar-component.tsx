de53006fd910a5771b1eff89b8c1a5da192a874c
Biplab jena <jbiplab80@gmail.com>
2025-09-05 11:37:41 +0530

    update

--- app/components/layout/sidebar-component.tsx
+++ app/components/layout/sidebar-component.tsx
@@ -47,16 +47,16 @@ export const SidebarComponent = ({
       href: `/members`,
       icon: Users,
     },
-    {
-      title: "Achieved",
-      href: `/achieved`,
-      icon: CheckCircle2,
-    },
-    {
-      title: "Settings",
-      href: "/settings",
-      icon: Settings,
-    },
+    // {
+    //   title: "Achieved",
+    //   href: `/achieved`,
+    //   icon: CheckCircle2,
+    // },
+    // {
+    //   title: "Settings",
+    //   href: "/settings",
+    //   icon: Settings,
+    // },
   ];
 
   return (
@@ -72,7 +72,7 @@ export const SidebarComponent = ({
             <div className="flex items-center gap-2">
               <Wrench className="size-6 text-blue-600" />
               <span className="font-semibold text-lg hidden md:block">
-                TaskHuh
+                TaskHub
               </span>
             </div>
           )}
